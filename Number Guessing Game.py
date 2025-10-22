91a0474b27661bfdc5eb9ffad7533c212e1469e4
Damien Hays <48137124+dahays@users.noreply.github.com>
2025-10-22 11:53:14 -0500

    Create Number Guessing Game.py
    Number Guessing Game Origin

---
+++ Number Guessing Game.py
@@ -0,0 +1,28 @@
+#This is an example main
+#main should be written before anything else
+def main():
+    choice = int(input("Choose a menu option: "))
+    
+    if choice == 1:
+        menu()
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    else:
+        print("Thanks.  Bye.")
+        
+def menu():
+    #menu accepts no arguments
+    #  it outputs the menu and returns choice
+    pass
+
+def get_range():
+    #  this does something
+    #  pass lets it run without crashing
+    pass
+
+def another_function():
+    #  This is another function
+    #  it does something and returns something
+    pass

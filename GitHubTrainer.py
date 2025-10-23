ede1cb414bd0ddb15074fb336f34dbc11a48f0aa
Damien Hays <48137124+dahays@users.noreply.github.com>
2025-10-23 12:44:16 -0500

    Create GitHubTrainer.py

---
+++ GitHubTrainer.py
@@ -0,0 +1,167 @@
+def main():
+    #  main accepts no arguments
+    #  it calls menu, and then processes
+    #  the returnes value to call each function
+    
+    choice = menu()
+    if choice == 1:
+        oliver(5)
+    elif choice == 2:
+        henry("test")
+    elif choice == 3:
+        tate("money")
+    elif choice == 4:
+        brody(2.5)
+    elif choice == 5:
+        cohen("love")
+    elif choice == 6:
+        gage(123)
+    elif choice == 7:
+        gabe("book")
+    elif choice == 8:
+        parker("werd")
+    elif choice == 9:
+        logan_k("U MAD BRO?")
+    elif choice == 10:
+        allen(67)
+    elif choice == 11:
+        logan_m("rock on")
+    elif choice == 12:
+        roman(3.14)
+    elif choice == 13:
+        sam(5)
+    elif choice == 14:
+        antonio("pony")
+    elif choice == 15:
+        bryson("9")
+    elif choice == 16:
+        tristan(19.91)
+    elif choice == 17:
+        bricen("ace")
+    elif choice == 18:
+        alex("sleep token")
+    elif choice == 19:
+        rhett("MCU")
+    
+    
+def menu():
+    #  menu accepts no arguments
+    #  it presents the user with a list of choices
+    #  and returns the chosen menu choice
+    
+    print("Please choose from the following choices:")
+    print("1.  Oliver")
+    print("2.  Henry")
+    print("3.  Tate")
+    print("4.  Brody")
+    print("5.  Cohen")
+    print("6.  Gage")
+    print("7.  Gabe")
+    print("8.  Parker")
+    print("9.  Logan K.")
+    print("10. Allen")
+    input("press any key for more...")
+    print("11. Logan M.")
+    print("12. Roman")
+    print("13. Sam")
+    print("14. Antonio")
+    print("15. Bryson")
+    print("16. Tristan")
+    print("17. Bricen")
+    print("18. Alex")
+    print("19. Rhett")
+    
+def oliver(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def henry(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def tate(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def brody(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def cohen(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def gage(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def gabe(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def parker(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def logan_k(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def allen(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def logan_m(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def roman(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def sam(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def antonio(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def bryson(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def tristan(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def bricen(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def alex(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something
+    
+def rhett(arg):
+    #  ____ accepts a ___ for arg
+    #  it does something
+    #  it returns something

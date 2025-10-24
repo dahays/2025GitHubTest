10b266ef9d160ceb76d19acc5cc494b6262b647f
AlexW2010 <askforarose11@gmail.com>
2025-10-24 12:43:25 -0500

    Alex. W Branch

--- GitHubTrainer.py
+++ GitHubTrainer.py
@@ -183,10 +183,12 @@ def bricen(arg):
     pass
     
 def alex(arg):
-    #  ____ accepts a ___ for arg
-    #  it does something
-    #  it returns something
-    pass
+    # accepts an argument (arg)
+    # it asks for a number and prints arg that many times
+    num = int(input("Enter a number: "))
+    for i in range(num):
+        print(arg)
+    return
     
 def rhett(arg):
     #  ____ accepts a ___ for arg
